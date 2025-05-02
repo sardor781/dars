94a593c134857857e2453e775992fd770bea238c
sardor781 <dastrx4@gmail.com>
2025-05-02 11:53:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let ism = prompt('Ismingizni kiriting: ');
+console.log('ism:', ism) ;
+let familiya = prompt("Familiyangizni kiriting: ");
+console.log("familiya:", familiya);
+
+let yosh = prompt('Yoshingizni kiriting: ');
+console.log('yosh:', yosh) ;
+let yoqtirgan , faningizni , yozing = prompt('Yoqtirgan faningizni yozing: ');
+console.log('yoqtirgan faningiz:', yozing) ;
+let yashaydigan , tumaningizni , kiriting = prompt('Yashaydigan tumani yozing: ');
+console.log('yashaydigan tumani:', kiriting) ;
+
+alert('malumotlaringizni kiritganingiz uchun raxmat!');
